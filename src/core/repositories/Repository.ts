ee170befc76bbb950747e9ddf47793249d23ea31
LucaPinheiro <lucapgomes11@gmail.com>
2024-03-25 10:36:36 -0300

    adjusting a dumb error✅

--- src/core/repositories/Repository.ts
+++ src/core/repositories/Repository.ts
@@ -1,49 +1,20 @@
-import { User } from "../../core/structure/entities/User";
-import { IUserRepo } from "../../core/repositories/interfaces/IUserRepo";
-import { UserMock } from "../structure/mocks/UserMock";
+import { UserRepo } from "./database/UserRepo";
+import { UserRepoMock } from "./mocks/UserRepoMock";
 
-export class UserRepoMock implements IUserRepo {
-  public user_mock: UserMock;
+class RepositoryProps {
+    user_repo: boolean = false;
+    project_repo: boolean = false;
+}
 
-  constructor() {
-    this.user_mock = new UserMock();
-  }
+export class Repository {
+    public UserRepo: UserRepo | UserRepoMock | null;
 
-  public get_user(id: string): Promise<User | null> {
-    return new Promise((resolve, reject) => {
-      const user = this.user_mock.users.find((user) => user.id === id);
-      resolve(user || null);
-    });
-  }
-
-  public create_user(user: User): Promise<boolean> {
-    this.user_mock.users.push(user);
-    return Promise.resolve(true);
-  }
-
-  public update_user(
-    userId: string,
-    course: string,
-    semester_course: number
-  ): Promise<User | null> {
-    return new Promise((resolve, reject) => {
-      const user_index = this.user_mock.users.findIndex(
-        (user) => user.id === userId
-      );
-      if (user_index === -1) {
-        resolve(null);
-      } else {
-        this.user_mock.users[user_index].course = course;
-        this.user_mock.users[user_index].semester_course = semester_course;
-        resolve(this.user_mock.users[user_index]);
-      }
-    });
-  }
-
-  public get_user_by_email(email: string): Promise<User | null> {
-    return new Promise((resolve, reject) => {
-      const user = this.user_mock.users.find((user) => user.email === email);
-      resolve(user || null);
-    });
-  }
+    constructor({ user_repo = false, project_repo = false }: RepositoryProps) {
+        if (user_repo) {
+            this.UserRepo = process.env.STAGE === 'test' ? new UserRepoMock() : new UserRepo();
+        }
+        if (project_repo) {
+            // this.ProjectRepo = process.env.STAGE === 'test' ? new ProjectRepoMock() : new ProjectRepo();
+        }
+    }
 }
