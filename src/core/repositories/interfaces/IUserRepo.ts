4c887707fab581f41ada47384f9360000c5f8b99
LucaPinheiro <lucapgomes11@gmail.com>
2024-03-25 07:25:13 -0300

    adjusting return method update user in interface

--- src/core/repositories/interfaces/IUserRepo.ts
+++ src/core/repositories/interfaces/IUserRepo.ts
@@ -2,7 +2,7 @@ import { User } from '../../structure/entities/User';
 
 export interface IUserRepo {
     create_user(user: User): Promise<boolean>;
-    update_user(user: User): Promise<User | null>;
+    update_user(userId: string, course: String, semester_course: number): Promise<User | null>;
     get_user(id: string): Promise<User | null>;
     get_user_by_email(email: string): Promise<User | null>;
 }
