03b5a4b923efbf4a8a3714a47b9da2b9d24bea57
Felipe Carillo <felipecarillo@outlook.com>
2024-03-26 09:13:58 -0300

    Changing the ProjectStatusEnum to use a string instead of a number

--- src/core/helpers/enums/ProjectStatusEnum.ts
+++ src/core/helpers/enums/ProjectStatusEnum.ts
@@ -1,9 +1,7 @@
 export enum ProjectStatusEnum {
-    TO_START, //When is not open to apply.
-    ACTIVE, //When is active and you student can apply.
-    ON_HOLD, //When active but not open to apply because of some problems, etc.
-    WAITING_FOR_RESULTS, //When closed to apply but results are not done.
-    ENDED, //When is closed to apply and results are out.
-    CANCELED, //When project is canceled, meaning it will not be happening.
-    NEEDS_MORE_DATA //When project is missing some data to be active.
+    TO_START="TO_START", //When is not open to apply.
+    ACTIVE="ACTIVE", //When is active and you student can apply.
+    ON_HOLD="ON_HOLD", //When finished the application period and is waiting for results.
+    ENDED="ENDED", //When is closed to apply and results are out.
+    CANCELED="CANCELED", //When project is canceled, meaning it will not be happening.
 }
