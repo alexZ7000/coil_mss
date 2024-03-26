6f1379871c024321c6861555e6260b277054912c
LucaPinheiro <lucapgomes11@gmail.com>
2024-03-26 20:17:18 -0300

    adjusting method update user

--- src/core/repositories/interfaces/IUserRepo.ts
+++ src/core/repositories/interfaces/IUserRepo.ts
@@ -1,8 +1,8 @@
-import { User } from '../../structure/entities/User';	
+import { User } from "../../structure/entities/User";
 
 export interface IUserRepo {
-    create_user(user: User): Promise<boolean>;
-    update_user(userId: string, course: String, semester_course: number): Promise<User>;
-    get_user(id: string): Promise<User | null>;
-    get_user_by_email(email: string): Promise<User | null>;
+  create_user(user: User): Promise<boolean>;
+  update_user(user: User): Promise<User>;
+  get_user(id: string): Promise<User | null>;
+  get_user_by_email(email: string): Promise<User | null>;
 }
