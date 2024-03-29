6d08df81ab4b8e74e41e6e00ba6d0d6a18e75a96
Felipe Carillo <felipecarillo@outlook.com>
2024-03-29 17:52:08 -0300

    Adding the dialect in sequelize

--- src/core/repositories/database/DatabaseMain.ts
+++ src/core/repositories/database/DatabaseMain.ts
@@ -8,29 +8,9 @@ export class DatabaseMain {
     public dynamo_dbclient: DynamoDBClient;
 
     constructor() {
-        if (process.env.STAG === 'dev') {
-            const env_vars = [
-                'RDS_HOSTNAME',
-                'RDS_PORT',
-                'RDS_DB_NAME',
-                'RDS_USERNAME',
-                'RDS_PASSWORD',
-                'RDS_DIALECT',
-                'PROJECT_TABLE',
-                'AWS_REGION'
-            ];
-            // Check if all environment variables are set
-            for (let env_var of env_vars) {
-                if (!process.env[env_var]) {
-                    throw new Error(`Missing ${env_var} environment variable.`);
-                }
-            }
-        }
-
-        const dialect: 'mysql' | 'mariadb' | 'postgres' | 'mssql' | 'sqlite' = process.env.RDS_DIALECT as any;
         this.project_table = process.env.PROJECT_TABLE as string;
         this.rd_client = new Sequelize({
-            dialect: dialect,
+            dialect: `mysql`,
             host: process.env.RDS_HOSTNAME,
             port: Number(process.env.RDS_PORT),
             username: process.env.RDS_USERNAME,
