bfcd404b1bb55a1c6d0666c6d4f6d8d6356cd43c
LucaPinheiro <lucapgomes11@gmail.com>
2024-04-01 21:59:15 -0300

    get user controller✅

---
+++ src/modules/get_user/app/get_user_controller.ts
@@ -0,0 +1,56 @@
+import { EntityError } from "../../../core/helpers/errors/EntityError";
+import {
+  InvalidParameter,
+  InvalidRequest,
+  MissingParameter,
+  UserNotAuthenticated,
+} from "../../../core/helpers/errors/ModuleError";
+import {
+  BadRequest,
+  HttpRequest,
+  HttpResponse,
+  InternalServerError,
+  OK,
+  ParameterError,
+  Unauthorized,
+} from "../../../core/helpers/http/http_codes";
+import { GetUserUsecase } from "./get_user_usecase";
+
+export class GetUserController {
+  public usecase: GetUserUsecase;
+
+  constructor(usecase: GetUserUsecase) {
+    this.usecase = usecase;
+  }
+
+  public async execute(request: HttpRequest): Promise<HttpResponse> {
+    try {
+      if (!request) {
+        throw new InvalidRequest();
+      }
+      if (!request.headers) {
+        throw new InvalidRequest("Headers");
+      }
+
+      let response = await this.usecase.execute(request.headers);
+      return new OK(response, "User found successfully");
+    } catch (error) {
+      if (error instanceof InvalidRequest) {
+        return new BadRequest(error.message);
+      }
+      if (error instanceof UserNotAuthenticated) {
+        return new Unauthorized(error.message);
+      }
+      if (error instanceof EntityError) {
+        return new ParameterError(error.message);
+      }
+      if (error instanceof InvalidParameter) {
+        return new ParameterError(error.message);
+      }
+      if (error instanceof MissingParameter) {
+        return new ParameterError(error.message);
+      }
+      return new InternalServerError(error.message);
+    }
+  }
+}
