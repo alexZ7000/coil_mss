7ad488fc602f6221edb0227d7fe5638a5235959c
Felipe Carillo <felipecarillo@outlook.com>
2024-04-16 17:34:07 -0300

    Updating event bridge.

--- src/core/helpers/functions/event_bridge.ts
+++ src/core/helpers/functions/event_bridge.ts
@@ -1,12 +1,20 @@
+import dotenv from 'dotenv';
 import { EventBridge, Lambda } from 'aws-sdk';
+
+dotenv.config();
+
 
 export class EventBridgeManager {
     private event: EventBridge;
     private lambda: Lambda;
+    private aws_region: string;
+    private aws_account_id: string;
 
     constructor() {
         this.event = new EventBridge();
         this.lambda = new Lambda();
+        this.aws_region = process.env.AWS_REGION as string;
+        this.aws_account_id = process.env.AWS_ACCOUNT_ID as string;
     }
 
     private async get_rule(rule_name: string): Promise<any | undefined> {
@@ -33,24 +41,31 @@ export class EventBridgeManager {
             }).promise()).Configuration?.FunctionArn as string;
 
             this.lambda.addPermission({
+                Action: "lambda:InvokeFunction",
                 FunctionName: lambda_function,
+                Principal: "events.amazonaws.com",
                 StatementId: rule_name,
-                Action: 'lambda:InvokeFunction',
-                Principal: 'events.amazonaws.com',
-                SourceArn: (await this.event.putRule({
-                    Name: rule_name,
-                    ScheduleExpression: `cron(${date.getMinutes()} ${date.getHours()} ${date.getDate()} ${date.getMonth() + 1} ? ${date.getFullYear()})`,
-                    State: 'ENABLED',
-                }).promise()).RuleArn,
+                SourceArn: "arn:aws:events:" + this.aws_region + ":" + this.aws_account_id + ":rule/" + rule_name,
+            }).promise();
+
+            this.event.putRule({
+                Name: rule_name,
+                ScheduleExpression: "cron(" + date.getMinutes() + " " + date.getHours() + " " + date.getDate() + " " + (date.getMonth() + 1) + " ? " + date.getFullYear() + ")",
+                State: "ENABLED",
             }).promise();
 
             this.event.putTargets({
                 Rule: rule_name,
                 Targets: [
                     {
-                        Arn: lambda_arn,
                         Id: rule_name,
-                        Input: JSON.stringify(payload),
+                        Arn: lambda_arn,
+                        InputTransformer: {
+                            InputPathsMap: {
+                                "body": "$.body",
+                            },
+                            InputTemplate: JSON.stringify(payload),
+                        },
                     },
                 ],
             }).promise();
@@ -65,6 +80,11 @@ export class EventBridgeManager {
             lambda_function = lambda_function + "_Coil";
 
             if (has_rule) {
+                this.lambda.removePermission({
+                    FunctionName: lambda_function,
+                    StatementId: rule_name,
+                }).promise();
+                
                 this.event.removeTargets({
                     Rule: rule_name,
                     Ids: [rule_name],
@@ -74,10 +94,6 @@ export class EventBridgeManager {
                     Name: rule_name,
                 }).promise();
 
-                this.lambda.removePermission({
-                    FunctionName: lambda_function,
-                    StatementId: rule_name,
-                }).promise();
                 return true;
             } else {
                 return true;
