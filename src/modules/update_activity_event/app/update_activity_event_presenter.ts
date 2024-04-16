9c50625b3b09235a1ccec4256c3a5606413bb89b
Felipe Carillo <felipecarillo@outlook.com>
2024-04-16 18:40:26 -0300

    Fixing update_activity_event_presenter.ts file

--- src/modules/update_activity_event/app/update_activity_event_presenter.ts
+++ src/modules/update_activity_event/app/update_activity_event_presenter.ts
@@ -5,6 +5,11 @@ const repository = new Repository({ activity_repo: true });
 
 
 export const handler = async (event: any, context: any) => {
-    const resp = await repository.ActivityRepo.update_activity_status(event.activity_id, event.status);
-    console.log(resp);
+    const body = event.body;
+    const resp = await repository.ActivityRepo.update_activity_status(body.activity_id, body.status);
+    if (resp) {
+        console.log("Activity updated successfully");
+    } else {
+        console.log("Activity not found");
+    }
 };
