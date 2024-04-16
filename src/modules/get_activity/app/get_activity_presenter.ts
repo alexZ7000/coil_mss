92f9ef1b2e7c1a748e249ce78e476df3781fa970
LucaPinheiro <lucapgomes11@gmail.com>
2024-04-16 09:59:28 -0300

    finishing presenter✅

--- src/modules/get_activity/app/get_activity_presenter.ts
+++ src/modules/get_activity/app/get_activity_presenter.ts
@@ -5,24 +5,9 @@ import { Repository } from "../../../core/repositories/Repository";
 
 const repository = new Repository({ activity_repo: true });
 
-const usecase = new GetActivityUsecase(repository.ActivityRepo);
+const usecase = new GetActivityUsecase(repository.UserRepo, repository.ActivityRepo);
 const controller = new GetActivityController(usecase);
 
 export const handler = async (event: any, context: any) => {
-  try {
-    const request = new HttpRequest(event);
-
-    const response = await controller.execute(request);
-
-    return {
-      statusCode: response.statusCode,
-      body: JSON.stringify(response.body),
-    };
-  } catch (error) {
-    console.error("Error:", error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ message: "Internal Server Error" }),
-    };
-  }
+  
 };
