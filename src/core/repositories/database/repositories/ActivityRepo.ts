d7add90f6f01b0946b0a6dcaf1afc936cca2788c
LucaPinheiro <lucapgomes11@gmail.com>
2024-04-23 15:40:48 -0300

    creating method get_activities_by_user_id✅

--- src/core/repositories/database/repositories/ActivityRepo.ts
+++ src/core/repositories/database/repositories/ActivityRepo.ts
@@ -1,195 +1,271 @@
-import { UniqueConstraintError, } from "sequelize";
+import { Op, UniqueConstraintError } from "sequelize";
 import { ActivityDTO } from "../dtos/ActivityDTO";
 import { User } from "../../../structure/entities/User";
 import { IActivityRepo } from "../../interfaces/IActivityRepo";
 import { Activity } from "../../../structure/entities/Activity";
 import { ActivityStatusEnum } from "../../../helpers/enums/ActivityStatusEnum";
+import { ActivityTypeEnum } from "../../../helpers/enums/ActivityTypeEnum";
 import {
-    Activity as ActivityDB, ActivityApplication, ActivityCourse, ActivityLanguage,
-    ActivityCriteria, ActivityPartnerInstitution, ActivityStatus, ActivityType,
-    Course, Institution, User as UserDB
+  Activity as ActivityDB,
+  ActivityApplication,
+  ActivityCourse,
+  ActivityLanguage,
+  ActivityCriteria,
+  ActivityPartnerInstitution,
+  ActivityStatus,
+  ActivityType,
+  User as UserDB,
 } from "../models/Models";
 
-
-
 export class ActivityRepo implements IActivityRepo {
+  private ActivityDTO: ActivityDTO;
 
-    private ActivityDTO: ActivityDTO;
+  constructor() {
+    this.ActivityDTO = new ActivityDTO();
+  }
 
-    constructor() {
-        this.ActivityDTO = new ActivityDTO();
+  async get_activity(id: string): Promise<Activity | null> {
+    const activity = await ActivityDB.findOne({
+      where: {
+        id: id,
+      },
+      include: [
+        { model: ActivityApplication, as: ActivityApplication.name },
+        { model: ActivityCourse, as: ActivityCourse.name },
+        { model: ActivityLanguage, as: ActivityLanguage.name },
+        { model: ActivityCriteria, as: ActivityCriteria.name },
+        {
+          model: ActivityPartnerInstitution,
+          as: ActivityPartnerInstitution.name,
+        },
+        { model: ActivityStatus, as: ActivityStatus.name },
+        { model: ActivityType, as: ActivityType.name },
+      ],
+    });
+
+    if (!activity) {
+      return null;
     }
 
-    async get_activity(id: string): Promise<Activity | null> {
-        const activity = await ActivityDB.findOne({
-            where: {
-                id: id
-            },
-            include: [
-                { model: ActivityApplication, as: ActivityApplication.name },
-                { model: ActivityCourse, as: ActivityCourse.name },
-                { model: ActivityLanguage, as: ActivityLanguage.name },
-                { model: ActivityCriteria, as: ActivityCriteria.name },
-                { model: ActivityPartnerInstitution, as: ActivityPartnerInstitution.name },
-                { model: ActivityStatus, as: ActivityStatus.name },
-                { model: ActivityType, as: ActivityType.name }
-            ]
-        });
+    return this.ActivityDTO.to_entity(activity.toJSON());
+  }
 
-        if (!activity) {
-            return null;
-        }
+  async get_activities_by_user_id(
+    user_id: string,
+    type: ActivityTypeEnum
+  ): Promise<Activity[] | null> {
+    const activities = await ActivityDB.findAll({
+      include: [
+        {
+          model: ActivityApplication,
+          as: ActivityApplication.name,
+          where: { user_id: user_id },
+        },
+        { model: ActivityCourse, as: ActivityCourse.name },
+        { model: ActivityLanguage, as: ActivityLanguage.name },
+        { model: ActivityCriteria, as: ActivityCriteria.name },
+        {
+          model: ActivityPartnerInstitution,
+          as: ActivityPartnerInstitution.name,
+        },
+        {
+          model: ActivityStatus,
+          as: ActivityStatus.name,
+          where: { id: { [Op.notLike]: ActivityStatusEnum.CANCELED } },
+        },
+        { model: ActivityType, as: ActivityType.name, where: { id: type } },
+      ],
+    });
 
-        return this.ActivityDTO.to_entity(activity.toJSON());
+    if (!activities) {
+      return null;
     }
 
-    async get_activities_by_user_id(user_id: string, type: ActivityStatusEnum): Promise<Activity[] | null> {
-        throw new Error("Method not implemented.");
+    return activities.map((activity) =>
+      this.ActivityDTO.to_entity(activity.toJSON())
+    );
+  }
+
+  async create_activity(activity: Activity): Promise<boolean> {
+    await ActivityDB.create({
+      id: activity.id,
+      title: activity.title,
+      description: activity.description,
+      status_id: activity.status_activity,
+      type_id: activity.type_activity,
+      start_date: activity.start_date,
+      end_date: activity.end_date,
+      created_at: activity.created_at,
+      updated_at: activity.updated_at,
+    });
+
+    await ActivityPartnerInstitution.bulkCreate(
+      activity.partner_institutions.map((institution) => ({
+        activity_id: activity.id,
+        institution_id: institution.id,
+      }))
+    );
+
+    await ActivityCourse.bulkCreate(
+      activity.courses.map((course) => ({
+        activity_id: activity.id,
+        course_id: course.id,
+      }))
+    );
+
+    await ActivityLanguage.bulkCreate(
+      activity.languages.map((language) => ({
+        activity_id: activity.id,
+        language: language,
+      }))
+    );
+
+    await ActivityCriteria.bulkCreate(
+      activity.criterias.map((criteria) => ({
+        activity_id: activity.id,
+        criteria: criteria.criteria,
+      }))
+    );
+    return true;
+  }
+
+  async check_activity_by_title(title: string): Promise<boolean> {
+    const activity = await ActivityDB.findOne({
+      where: {
+        title: title,
+      },
+      attributes: ["id"],
+    });
+
+    if (!activity) {
+      return false;
     }
 
-    async create_activity(activity: Activity): Promise<boolean> {
-        await ActivityDB.create({
-            id: activity.id,
-            title: activity.title,
-            description: activity.description,
-            status_id: activity.status_activity,
-            type_id: activity.type_activity,
-            start_date: activity.start_date,
-            end_date: activity.end_date,
-            created_at: activity.created_at,
-            updated_at: activity.updated_at,
-        });
+    return true;
+  }
 
-        await ActivityPartnerInstitution.bulkCreate(activity.partner_institutions.map(institution => ({
-            activity_id: activity.id,
-            institution_id: institution.id
-        })));
+  async get_all_activities_by_status(
+    status: ActivityStatusEnum | ActivityStatusEnum[]
+  ): Promise<Activity[]> {
+    throw new Error("Method not implemented.");
+  }
 
-        await ActivityCourse.bulkCreate(activity.courses.map(course => ({
-            activity_id: activity.id,
-            course_id: course.id
-        })));
+  async get_all_activities(): Promise<Activity[]> {
+    throw new Error("Method not implemented.");
+  }
 
-        await ActivityLanguage.bulkCreate(activity.languages.map(language => ({
-            activity_id: activity.id,
-            language: language
-        })));
+  async get_users_assigned_to_activity(activity_id: string): Promise<User[]> {
+    throw new Error("Method not implemented.");
+  }
 
-        await ActivityCriteria.bulkCreate(activity.criterias.map(criteria => ({
-            activity_id: activity.id,
-            criteria: criteria.criteria
-        })));
-        return true;
-    }
+  async assign_user_to_activity(
+    activity_id: string,
+    user_id: string
+  ): Promise<boolean> {
+    throw new Error("Method not implemented.");
+  }
 
-    async check_activity_by_title(title: string): Promise<boolean> {
-        const activity = await ActivityDB.findOne({
-            where: {
-                title: title
-            },
-            attributes: ['id']
-        });
+  async remove_user_from_activity(
+    activity_id: string,
+    user_id: string
+  ): Promise<boolean> {
+    throw new Error("Method not implemented.");
+  }
 
-        if (!activity) {
-            return false;
-        }
+  async update_activity(activity: Activity): Promise<boolean> {
+    await ActivityDB.update(
+      {
+        title: activity.title,
+        description: activity.description,
+        status_id: activity.status_activity,
+        type_id: activity.type_activity,
+        start_date: activity.start_date,
+        end_date: activity.end_date,
+        updated_at: activity.updated_at,
+      },
+      {
+        where: {
+          id: activity.id,
+        },
+      }
+    );
 
-        return true;
-    }
+    await ActivityPartnerInstitution.destroy({
+      where: {
+        activity_id: activity.id,
+      },
+    });
 
-    async get_all_activities_by_status(status: ActivityStatusEnum | ActivityStatusEnum[]): Promise<Activity[]> {
-        throw new Error("Method not implemented.");
-    }
+    await ActivityCourse.destroy({
+      where: {
+        activity_id: activity.id,
+      },
+    });
 
-    async get_all_activities(): Promise<Activity[]> {
-        throw new Error("Method not implemented.");
-    }
+    await ActivityLanguage.destroy({
+      where: {
+        activity_id: activity.id,
+      },
+    });
 
-    async get_users_assigned_to_activity(activity_id: string): Promise<User[]> {
-        throw new Error("Method not implemented.");
-    }
+    await ActivityCriteria.destroy({
+      where: {
+        activity_id: activity.id,
+      },
+    });
 
-    async assign_user_to_activity(activity_id: string, user_id: string): Promise<boolean> {
-        throw new Error("Method not implemented.");
-    }
+    await ActivityPartnerInstitution.bulkCreate(
+      activity.partner_institutions.map((institution) => ({
+        activity_id: activity.id,
+        institution_id: institution.id,
+      }))
+    );
 
-    async remove_user_from_activity(activity_id: string, user_id: string): Promise<boolean> {
-        throw new Error("Method not implemented.");
-    }
+    await ActivityCourse.bulkCreate(
+      activity.courses.map((course) => ({
+        activity_id: activity.id,
+        course_id: course.id,
+      }))
+    );
 
-    async update_activity(activity: Activity): Promise<boolean> {
-        await ActivityDB.update({
-            title: activity.title,
-            description: activity.description,
-            status_id: activity.status_activity,
-            type_id: activity.type_activity,
-            start_date: activity.start_date,
-            end_date: activity.end_date,
-            updated_at: activity.updated_at,
-        }, {
-            where: {
-                id: activity.id
-            }
-        });
+    await ActivityLanguage.bulkCreate(
+      activity.languages.map((language) => ({
+        activity_id: activity.id,
+        language: language,
+      }))
+    );
 
-        await ActivityPartnerInstitution.destroy({
-            where: {
-                activity_id: activity.id
-            }
-        });
+    await ActivityCriteria.bulkCreate(
+      activity.criterias.map((criteria) => ({
+        activity_id: activity.id,
+        criteria: criteria.criteria,
+      }))
+    );
+    return true;
+  }
 
-        await ActivityCourse.destroy({
-            where: {
-                activity_id: activity.id
-            }
-        });
+  async update_user_activity_status(
+    activity_id: string,
+    user_id: string,
+    status: ActivityStatusEnum
+  ): Promise<boolean> {
+    throw new Error("Method not implemented.");
+  }
 
-        await ActivityLanguage.destroy({
-            where: {
-                activity_id: activity.id
-            }
-        });
-
-        await ActivityCriteria.destroy({
-            where: {
-                activity_id: activity.id
-            }
-        });
-
-        await ActivityPartnerInstitution.bulkCreate(activity.partner_institutions.map(institution => ({
-            activity_id: activity.id,
-            institution_id: institution.id
-        })));
-
-        await ActivityCourse.bulkCreate(activity.courses.map(course => ({
-            activity_id: activity.id,
-            course_id: course.id
-        })));
-
-        await ActivityLanguage.bulkCreate(activity.languages.map(language => ({
-            activity_id: activity.id,
-            language: language
-        })));
-
-        await ActivityCriteria.bulkCreate(activity.criterias.map(criteria => ({
-            activity_id: activity.id,
-            criteria: criteria.criteria
-        })));
-        return true;
-    }
-
-    async update_user_activity_status(activity_id: string, user_id: string, status: ActivityStatusEnum): Promise<boolean> {
-        throw new Error("Method not implemented.");
-    }
-
-    async update_activity_status(activity_id: string, status: ActivityStatusEnum): Promise<boolean> {
-        await ActivityDB.update({
-            status_id: status
-        }, {
-            where: {
-                id: activity_id
-            }
-        });
-        return true;
-    }
+  async update_activity_status(
+    activity_id: string,
+    status: ActivityStatusEnum
+  ): Promise<boolean> {
+    await ActivityDB.update(
+      {
+        status_id: status,
+      },
+      {
+        where: {
+          id: activity_id,
+        },
+      }
+    );
+    return true;
+  }
 }
