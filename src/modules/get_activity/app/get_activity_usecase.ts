f7e1d6e127ae0a6d7fdeb163f295e86bfe40b492
LucaPinheiro <lucapgomes11@gmail.com>
2024-04-26 15:58:44 -0300

    Fixing error handling in GetActivityUsecase

--- src/modules/get_activity/app/get_activity_usecase.ts
+++ src/modules/get_activity/app/get_activity_usecase.ts
@@ -8,6 +8,7 @@ import {
 } from "../../../core/helpers/errors/ModuleError";
 import { TokenAuth } from "../../../core/helpers/functions/token_auth";
 import { NotFound } from "../../../core/helpers/http/http_codes";
+import { NotFoundError } from "../../../core/helpers/errors/RepoError";
 import { IActivityRepo } from "../../../core/repositories/interfaces/IActivityRepo";
 import { IUserRepo } from "../../../core/repositories/interfaces/IUserRepo";
 
@@ -22,7 +23,10 @@ export class GetActivityUsecase {
     this.activity_repo = activity_repo;
   }
 
-  async execute(headers: { [key: string]: any }, queryStringParameters: { [key: string]: any }) {
+  async execute(
+    headers: { [key: string]: any },
+    queryStringParameters: { [key: string]: any }
+  ) {
     if (!headers) {
       throw new InvalidRequest("Headers");
     }
@@ -50,18 +54,37 @@ export class GetActivityUsecase {
       throw new UserNotAuthenticated();
     }
 
-    const activity = await this.activity_repo.get_activity(queryStringParameters.activity_id);
+    const activity = await this.activity_repo.get_activity(
+      queryStringParameters.activity_id
+    );
     if (!activity) {
-      throw new NotFound("Activity not found");
+      throw new NotFoundError("Activity not found");
     }
 
     if (user.user_type === UserTypeEnum.STUDENT) {
-      const check_user_enrolled = this.activity_repo.check_activity_enrolled_by_user(user_id, activity.id);
-      const condition = ActivityStatusEnum.CANCELED === activity.status_activity || (!check_user_enrolled && [ActivityStatusEnum.ON_HOLD, ActivityStatusEnum.ENDED].includes(activity.status_activity))
+      const check_user_enrolled =
+        await this.activity_repo.check_activity_enrolled_by_user(
+          user_id,
+          activity.id
+        );
+      const condition =
+        ActivityStatusEnum.CANCELED === activity.status_activity ||
+        (!check_user_enrolled &&
+          [ActivityStatusEnum.ON_HOLD, ActivityStatusEnum.ENDED].includes(
+            activity.status_activity
+          ));
       if (condition) {
         throw new NotFound("Activity not found");
       }
     }
-    return activity.to_json();
+
+    const enrolled_users = await this.activity_repo.get_users_assigned_to_activity(
+      activity.id
+    );
+
+    return {
+      activity: activity.to_json(),
+      enrolled_users: enrolled_users.map((user) => user.to_json()),
+    };
   }
 }
