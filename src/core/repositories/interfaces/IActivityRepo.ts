703e0c694dd640383a132342c3b4f08bba97106c
Felipe Carillo <felipecarillo@outlook.com>
2024-04-27 10:58:21 -0300

    Add check_activity_by_id method to IActivityRepo interface

--- src/core/repositories/interfaces/IActivityRepo.ts
+++ src/core/repositories/interfaces/IActivityRepo.ts
@@ -7,9 +7,11 @@ export interface IActivityRepo {
     create_activity(activity: Activity): Promise<boolean>
     update_activity(activity: Activity): Promise<boolean>
     update_activity_status(activity_id: string, status: ActivityStatusEnum): Promise<boolean>
-
-    get_activity(id: string): Promise<Activity | null>
+    
+    check_activity_by_id(id: string): Promise<boolean>
     check_activity_by_title(title: string): Promise<boolean> 
+
+    get_activity(id: string, applicants?: boolean): Promise<Activity | null>
     get_users_assigned_to_activity(activity_id: string): Promise<User[]>
     get_activities_by_user_id(user_id: string, type: ActivityStatusEnum): Promise<Activity[] | null>
     get_all_activities_by_status(status: ActivityStatusEnum | ActivityStatusEnum[], type: ActivityTypeEnum): Promise<Activity[] | null> 
