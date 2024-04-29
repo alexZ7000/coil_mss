a65a27b86fdfe8936b3217b5b7507f91b43e7efd
Ivan Petrov <ivan.p.petrov02@gmail.com>
2024-04-29 22:39:28 +0300

    test

--- test/modules/get_all_courses/app/get_all_courses.test.ts
+++ test/modules/get_all_courses/app/get_all_courses.test.ts
@@ -8,12 +8,12 @@ import { TokenAuth } from '../../../../src/core/helpers/functions/token_auth';
 describe("Testing getting all courses", () => {
   const user_admin = new UserMock().users[0];
   const user_student = new UserMock().users[1];
-  const user_moderator = new UserMock().users[2];
 
   it("Should return a success message", async () => {
     let courses = new CourseMock().courses;
     let course = courses[0];
     let token = (await new TokenAuth().generate_token(user_admin.id));
+    console.log('Token:', token);
     const event = {
       headers: {
         Authorization: token,
@@ -24,12 +24,14 @@ describe("Testing getting all courses", () => {
       }),
     };
     const response = await handler(event, null);
+    console.log('Response:', response);
     //expect(response.statusCode).toBe(200);
     expect(JSON.parse(response.body).message).toBe("User authenticated successfully");
   });
 
   it("Should return a not found error", async () => {
     let token = (await new TokenAuth().generate_token(user_admin.id));
+    console.log('Token:', token);
     const event = {
       headers: {
         Authorization: token,
@@ -40,6 +42,7 @@ describe("Testing getting all courses", () => {
       }),
     };
     const response = await handler(event, null);
+    console.log('Response:', response);
     //expect(response.statusCode).toBe(404);
     expect(JSON.parse(response.body).message).toBe("User or Course not found");
   });
