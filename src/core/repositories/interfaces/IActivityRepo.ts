0bb072d74fd9799518e6b17d6d24a8c6cc438e1d
LucaPinheiro <lucapgomes11@gmail.com>
2024-05-08 09:03:58 -0300

    Refactor IActivityRepo interface and update activity methods

--- src/core/repositories/interfaces/IActivityRepo.ts
+++ src/core/repositories/interfaces/IActivityRepo.ts
@@ -5,15 +5,17 @@ import { ActivityStatusEnum } from "../../helpers/enums/ActivityStatusEnum";
 
 export interface IActivityRepo {
     create_activity(activity: Activity): Promise<boolean>
-
-    get_activity(id: string): Promise<Activity | null>
-    get_activity_by_title(title: string): Promise<Activity | null> 
-    get_users_assigned_to_activity(activity_id: string): Promise<User[]>
+    update_activity(activity: Activity): Promise<boolean>
+    update_activity_status(activity_id: string, status: ActivityStatusEnum): Promise<boolean>
+    
+    check_activity_by_id(id: string): Promise<boolean>
+    check_activity_by_title(title: string): Promise<boolean> 
     check_activity_enrolled_by_user(user_id: string, activity_id: string): Promise<boolean>
-    get_activities_by_user_id(user_id: string, activity_type: ActivityTypeEnum): Promise<Activity[] | null>
+
+    get_activity(id: string, applicants?: boolean): Promise<Activity | null>
+    get_activities_by_user_id(user_id: string, type: ActivityTypeEnum): Promise<Activity[] | null>
     get_all_activities_by_status(status: ActivityStatusEnum | ActivityStatusEnum[], type: ActivityTypeEnum): Promise<Activity[] | null> 
     
-    assign_user_to_activity(activity_id: string, user_id: string): Promise<boolean>
-    remove_user_from_activity(activity_id: string, user_id: string): Promise<boolean>
-    update_user_activity_status(activity_id: string, user_id: string, status: ActivityStatusEnum): Promise<boolean>
+    assign_user_to_activity(activity_id: string, user_id: string): Promise<{ assign: boolean }>
+    update_user_activity_status(activity_id: string, user_id: string, status: boolean): Promise<boolean>
 }
