77a0b3e97bd8564b5314119801db6f8e6c5465ac
FelipeCarillo <felipecarillo@outlook.com>
2024-05-09 15:29:15 -0300

    chore: Update get_institution_controller.ts
    Refactor the import statements in `get_institution_controller.ts` to include the new error classes for user authentication and not found errors.

--- src/modules/get_institution/app/get_institution_controler.ts
+++ src/modules/get_institution/app/get_institution_controler.ts
@@ -1,8 +1,8 @@
 import { GetInstitutionUsecase } from "./get_institution_usecase";
 
 import { EntityError } from '../../../core/helpers/errors/EntityError';
-import { Conflict, OK, Unauthorized, BadRequest, HttpRequest, HttpResponse} from '../../../core/helpers/http/http_codes';
-import { ConflictError, InvalidParameter, InvalidRequest, MissingParameter, UserNotAuthenticated } from '../../../core/helpers/errors/ModuleError';
+import { Conflict, OK, Unauthorized, BadRequest, HttpRequest, HttpResponse, Forbidden, NotFound} from '../../../core/helpers/http/http_codes';
+import { ConflictError, InvalidParameter, InvalidRequest, MissingParameter, NotfoundError, UserNotAllowed, UserNotAuthenticated } from '../../../core/helpers/errors/ModuleError';
 import { ParameterError, InternalServerError } from '../../../core/helpers/http/http_codes';
 
 
@@ -29,7 +29,7 @@ export class GetInstitutionController {
                 throw new InvalidRequest("Body");
             }
 
-            let response = await this.usecase.execute(request.body.queryStringParameters, request.headers);
+            let response = await this.usecase.execute(request.body.body, request.headers);
             return new OK(response, "Institution found");
 
         } catch (error) {
@@ -39,8 +39,11 @@ export class GetInstitutionController {
             if (error instanceof UserNotAuthenticated) {
                 return new Unauthorized(error.message);
             }
-            if (error instanceof ConflictError) {
-                return new Conflict(error.message);
+            if (error instanceof UserNotAllowed) {
+                return new Forbidden(error.message);
+            }
+            if (error instanceof NotfoundError) {
+                return new NotFound(error.message);
             }
             if (error instanceof EntityError) {
                 return new ParameterError(error.message);
