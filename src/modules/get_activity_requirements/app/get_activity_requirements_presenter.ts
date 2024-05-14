40bdc73dd181030010a4421323730e13319d2c28
FelipeCarillo <felipecarillo@outlook.com>
2024-05-13 23:23:46 -0300

    refactor: Update GetActivityRequirementsUsecase to include institution_repo in constructor

--- src/modules/get_activity_requirements/app/get_activity_requirements_presenter.ts
+++ src/modules/get_activity_requirements/app/get_activity_requirements_presenter.ts
@@ -3,9 +3,9 @@ import { HttpRequest } from "../../../core/helpers/http/http_codes";
 import { GetActivityRequirementsUsecase } from "./get_activity_requirements_usecase";
 import { GetActivityRequirementsController } from "./get_activity_requirements_controller";
 
-const repository = new Repository({ user_repo: true, language_repo: true, course_repo: true, criteria_repo: true });
+const repository = new Repository({ user_repo: true, institution_repo: true, language_repo: true, course_repo: true, criteria_repo: true });
 
-const usecase = new GetActivityRequirementsUsecase(repository.UserRepo, repository.CourseRepo, repository.CriteriaRepo, repository.LanguageRepo);
+const usecase = new GetActivityRequirementsUsecase(repository.UserRepo, repository.InstitutionRepo, repository.CourseRepo, repository.CriteriaRepo, repository.LanguageRepo);
 
 const controller = new GetActivityRequirementsController(usecase);
 
