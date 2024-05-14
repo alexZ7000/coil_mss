b6e7f5e03723c302b0d2cef1c343a8629f971624
FelipeCarillo <felipecarillo@outlook.com>
2024-05-13 23:22:27 -0300

    refactor: Update InstitutionRepo to include method for retrieving all institution names

--- src/core/repositories/database/repositories/InstitutionRepo.ts
+++ src/core/repositories/database/repositories/InstitutionRepo.ts
@@ -1,7 +1,12 @@
 import { InstitutionDTO } from "../dtos/InstitutionDTO";
 import { IInstitutionRepo } from "../../interfaces/IInstitutionRepo";
 import { Institution } from "../../../structure/entities/Institution";
-import { Institution as InstitutionDB, InstitutionImage as InstitutionImageDB, InstitutionSocialMedia as InstitutionSocialMediaDB } from "../models/Models";
+import {
+    InstitutionCountry, Institution as InstitutionDB, InstitutionImage as InstitutionImageDB,
+    InstitutionSocialMedia as InstitutionSocialMediaDB, Country as CountryDB, SocialMedia as SocialMediaDB
+} from "../models/Models";
+import { Country } from "../../../structure/entities/Country";
+import { SocialMedia } from "../../../structure/entities/SocialMedia";
 
 
 export class InstitutionRepo implements IInstitutionRepo {
@@ -21,6 +26,20 @@ export class InstitutionRepo implements IInstitutionRepo {
         return institution_found ? true : false;
     }
 
+    public async get_all_institutions_names(): Promise<{ id: string, name: string }[]> {
+        const institutions_found = await InstitutionDB.findAll({
+            attributes: ['id', 'name']
+        });
+
+        return institutions_found.map(institution => {
+            let institution_json = institution.toJSON();
+            return {
+                id: institution_json.id,
+                name: institution_json.name
+            };
+        });
+    }
+
     public async get_all_institutions(): Promise<Institution[]> {
         let institutions_found = await InstitutionDB.findAll({
             include: [
@@ -38,16 +57,24 @@ export class InstitutionRepo implements IInstitutionRepo {
             id: institution.id,
             name: institution.name,
             description: institution.description,
-            email: institution.email,
-            country: institution.country
+            email: institution.email
         });
-        await InstitutionSocialMediaDB.bulkCreate(institution.social_medias.map((sm: { media: string; link: string; }) => {
+
+        await InstitutionCountry.bulkCreate(institution.countries.map((country: { id: number, country?: Country }) => {
             return {
                 institution_id: institution.id,
-                media: sm.media,
+                country_id: country.id
+            }
+        }));
+
+        await InstitutionSocialMediaDB.bulkCreate(institution.social_medias.map((sm: { id: number, media?: SocialMedia, link: string }) => {
+            return {
+                institution_id: institution.id,
+                social_media_id: sm.id,
                 link: sm.link
             }
         }));
+
         await InstitutionImageDB.bulkCreate(institution.images.map((img: string) => {
             return {
                 institution_id: institution.id,
@@ -63,29 +90,45 @@ export class InstitutionRepo implements IInstitutionRepo {
             name: institution.name,
             description: institution.description,
             email: institution.email,
-            country: institution.country
         }, {
             where: {
                 id: institution.id
             }
         });
+
         await InstitutionSocialMediaDB.destroy({
             where: {
                 institution_id: institution.id
             }
         });
+
         await InstitutionImageDB.destroy({
             where: {
                 institution_id: institution.id
             }
         });
-        await InstitutionSocialMediaDB.bulkCreate(institution.social_medias.map((sm: { media: string; link: string; }) => {
+
+        await InstitutionCountry.destroy({
+            where: {
+                institution_id: institution.id
+            }
+        });
+
+        await InstitutionSocialMediaDB.bulkCreate(institution.social_medias.map((sm: { id: number, media?: SocialMedia, link: string }) => {
             return {
                 institution_id: institution.id,
                 media: sm.media,
                 link: sm.link
             }
         }));
+
+        await InstitutionCountry.bulkCreate(institution.countries.map((country: { id: number, country?: Country }) => {
+            return {
+                institution_id: institution.id,
+                country_id: country.id
+            }
+        }));
+
         await InstitutionImageDB.bulkCreate(institution.images.map((img: string) => {
             return {
                 institution_id: institution.id,
@@ -112,8 +155,9 @@ export class InstitutionRepo implements IInstitutionRepo {
                 id: id
             },
             include: [
-                { model: InstitutionSocialMediaDB, as: 'social_medias' },
+                { model: InstitutionSocialMediaDB, as: 'social_medias', include: [{ model: SocialMediaDB, as: 'media' }] },
                 { model: InstitutionImageDB, as: 'images' },
+                { model: InstitutionCountry, as: 'countries', include: [{ model: CountryDB, as: 'country' }] }
             ]
         });
 
