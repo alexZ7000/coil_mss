61e95420e533eff4c7b7c94ad3767938f1f69bdf
FelipeCarillo <felipecarillo@outlook.com>
2024-05-15 19:13:48 -0300

    refactor: Update CreateInstitutionController to handle InvalidRequest and InvalidParameter errors

--- src/modules/create_institution/app/create_institution_controller.ts
+++ src/modules/create_institution/app/create_institution_controller.ts
@@ -4,7 +4,7 @@ import { Institution } from "../../../core/structure/entities/Institution";
 import { EntityError } from '../../../core/helpers/errors/EntityError';
 import { Created, Forbidden, HttpRequest, HttpResponse, Unauthorized, Unprocessable_Entity } from '../../../core/helpers/http/http_codes';
 import { BadRequest, ParameterError, InternalServerError } from '../../../core/helpers/http/http_codes';
-import { MissingParameter, UserNotAllowed, UserNotAuthenticated } from '../../../core/helpers/errors/ModuleError';
+import { InvalidParameter, InvalidRequest, MissingParameter, UserNotAllowed, UserNotAuthenticated } from '../../../core/helpers/errors/ModuleError';
 import { UniqueConstraintError } from "sequelize";
 import { UnauthenticatedAction } from "aws-cdk-lib/aws-elasticloadbalancingv2";
 
@@ -18,21 +18,27 @@ export class CreateInstitutionController {
     async execute(request: HttpRequest): Promise<HttpResponse> {
         try {
             if (!request) {
-                throw new MissingParameter("Request");
+                throw new InvalidRequest();
             }
             if (!request.body) {
-                throw new MissingParameter("Body");
+                throw new InvalidRequest("Body");
             }
             if (!request.headers) {
-                throw new MissingParameter("Headers");
+                throw new InvalidRequest("Headers");
             }
 
             const usecase = await this.usecase.execute(request.body.body, request.headers);
             return new Created(usecase, "Institution created successfully");
 
         } catch (error: any) {
-            if (error instanceof EntityError) {
+            if (error instanceof InvalidRequest) {
                 return new BadRequest(error.message);
+            }
+            if (error instanceof EntityError) {
+                return new ParameterError(error.message);
+            }
+            if (error instanceof InvalidParameter) {
+                return new ParameterError(error.message);
             }
             if (error instanceof MissingParameter) {
                 return new ParameterError(error.message);
