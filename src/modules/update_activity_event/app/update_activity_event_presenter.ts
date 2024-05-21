084a168530fa5b8e43f4d78f6d690575412e1aa9
FelipeCarillo <felipecarillo@outlook.com>
2024-05-21 09:00:24 -0300

    refactor: Update UpdateActivityEventPresenter to handle activity status updates and trigger event deletion

--- src/modules/update_activity_event/app/update_activity_event_presenter.ts
+++ src/modules/update_activity_event/app/update_activity_event_presenter.ts
@@ -1,12 +1,18 @@
 import { Repository } from "../../../core/repositories/Repository";
+import { EventBridgeManager } from "../../../core/helpers/functions/event_bridge";
+import { ActivityStatusEnum } from "../../../core/helpers/enums/ActivityStatusEnum";
 
 const repository = new Repository({ activity_repo: true });
 
 export const handler = async (event: any, context: any) => {
-    const body = event.body;
+    const event_bridge = new EventBridgeManager();
+    const body: { activity_id: string; status: ActivityStatusEnum } = JSON.parse(event.body);
+    console.log("Activity ID: ", body.activity_id + " Status: " + body.status);
     const resp = await repository.ActivityRepo.update_activity_status(body.activity_id, body.status);
     if (resp) {
         console.log("Activity updated successfully");
+        let event_name = body.status === ActivityStatusEnum.ACTIVE ? "START_ACTIVITY_" : "END_ACTIVITY_";
+        await event_bridge.delete_trigger(event_name + body.activity_id, "Update_Activity_Event");
     } else {
         console.log("Activity not found");
     }
