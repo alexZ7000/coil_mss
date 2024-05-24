9a32897f2b78598a2c837747f34ce6012a272a22
FelipeCarillo <felipecarillo@outlook.com>
2024-05-24 10:39:51 -0300

    refactor: Add GetAllActivitiesCatalog feature

---
+++ src/modules/get_all_activities_catalog/app/get_all_activities_catalog_controller.ts
@@ -0,0 +1,45 @@
+import {
+  HttpRequest,
+  HttpResponse,
+  OK,
+  BadRequest,
+  Unauthorized,
+  ParameterError,
+  InternalServerError,
+} from "../../../core/helpers/http/http_codes";
+import {
+  InvalidRequest,
+  MissingParameter,
+  UserNotAuthenticated,
+} from "../../../core/helpers/errors/ModuleError";
+import { GetAllActivitiesCatalogUsecase } from "./get_all_activities_catalog_usecase";
+
+
+export class GetAllActivitiesCatalogController {
+  public usecase: GetAllActivitiesCatalogUsecase;
+
+  constructor(usecase: GetAllActivitiesCatalogUsecase) {
+    this.usecase = usecase;
+  }
+
+  public async execute(request: HttpRequest): Promise<HttpResponse> {
+    try {
+      if (!request) {
+        throw new InvalidRequest();
+      }
+      const response = await this.usecase.execute();
+      return new OK(response, "Activities found successfully");
+    } catch (error: any) {
+      if (error instanceof InvalidRequest) {
+        return new BadRequest(error.message);
+      }
+      if (error instanceof UserNotAuthenticated) {
+        return new Unauthorized(error.message);
+      }
+      if (error instanceof MissingParameter) {
+        return new ParameterError(error.message);
+      }
+      return new InternalServerError(error.message);
+    }
+  }
+}
---
+++ src/modules/get_all_activities_catalog/app/get_all_activities_catalog_usecase.ts
@@ -0,0 +1,30 @@
+import { TokenAuth } from "../../../core/helpers/functions/token_auth";
+import { IUserRepo } from "../../../core/repositories/interfaces/IUserRepo";
+import { ActivityTypeEnum } from "../../../core/helpers/enums/ActivityTypeEnum";
+import { IActivityRepo } from "../../../core/repositories/interfaces/IActivityRepo";
+
+
+export class GetAllActivitiesCatalogUsecase {
+  public token_auth: TokenAuth;
+  public activity_repo: IActivityRepo; 
+
+  constructor(activity_repo: IActivityRepo) {
+    this.token_auth = new TokenAuth();
+    this.activity_repo = activity_repo;
+  }
+
+  async execute() {
+
+    const activities = await this.activity_repo.get_all_activities_catalog();
+
+    const projects = activities ? activities.filter((activity) => activity.type_activity === ActivityTypeEnum.PROJECT) : null;
+    const mobilities = activities ? activities.filter((activity) => activity.type_activity === ActivityTypeEnum.ACADEMIC_MOBILITY) : null;
+
+    const response = {
+      projects: projects,
+      mobilities: mobilities
+    }
+
+    return response;
+  }
+}
